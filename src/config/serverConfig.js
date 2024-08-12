068eda8d3b0ba884b868b516827fcd6e9fa8d2c1
unknown <inderece8031@gmail.com>
2024-08-13 01:41:35 +0530

    Added HTTP URL paths for communication between BookingService and {FlightsAndSearchService, AuthService}

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -5,4 +5,7 @@ dotenv.config();
 module.exports = {
     PORT: process.env.PORT,
     DB_SYNC: process.env.DB_SYNC,
+    FLIGHT_SERVICE_PATH: process.env.FLIGHT_SERVICE_PATH,
+    UPDATE_FLIGHT_SERVICE_PATH: process.env.UPDATE_FLIGHT_SERVICE_PATH,
+    AUTH_SERVICE_PATH: process.env.AUTH_SERVICE_PATH,
 }
