1978266c24444b89d11a8ebdbd23d70916dbd5c8
unknown <inderece8031@gmail.com>
2024-08-13 01:31:41 +0530

    Added controller code for creat functionality

---
+++ src/controllers/booking-controller.js
@@ -0,0 +1,27 @@
+const { StatusCodes } = require('http-status-codes');
+const { BookingService } = require('../services/index');
+
+const bookingService = new BookingService();
+
+const create = async (req, res) => {
+    try {
+        const booking = await bookingService.createBooking(req.body);
+        return res.status(StatusCodes.OK).json({
+            data: booking,
+            success: true,
+            message: "Booking Successful",
+            error: {}
+        });
+    } catch (error) {
+        return res.status(error.statusCode).json({
+            data: {},
+            success: false,
+            message: error.message,
+            error: error.reason
+        });
+    }
+}
+
+module.exports = {
+    create,
+}
