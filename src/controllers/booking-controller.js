18cbebd3a4e986a610c6a7fd680189073c9a77fd
unknown <inderece8031@gmail.com>
2024-08-15 16:55:11 +0530

    Added update controller

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -22,6 +22,26 @@ const create = async (req, res) => {
     }
 }
 
+const update = async (req, res) => {
+    try {
+        const booking = await bookingService.updateBooking(req.params.id, req.body);
+        return res.status(StatusCodes.OK).json({
+            data: booking,
+            success: true,
+            message: "Booking updation Successful",
+            error: {}
+        });
+    } catch (error) {
+        return res.status(error.statusCode).json({
+            data: {},
+            success: false,
+            message: error.message,
+            error: error.reason
+        });
+    }
+}
+
 module.exports = {
     create,
+    update,
 }
